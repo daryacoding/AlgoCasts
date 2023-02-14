9f875642d287af6d4d2bf3c7a9f414a921e8bb72
daryacoding <abdullahzadeh.darya@gmail.com>
2023-02-13 21:01:07 -0500

    anagrams exercise solved

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,22 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const modifiedString = (theString) => {
+        //create a punctuation array to delete all punctuation from the string
+        let punctuation = ['!', '.', ',', ' ']
+        for (let i = 0; i < punctuation.length; i++) {
+            theString = theString.replaceAll(punctuation[i], '').toLowerCase()
+        }
+        // sort the string in alphabetical order
+        theString = theString.split('').sort()
+        theString = theString.join('')
+        return theString
+    }
+    stringA = modifiedString(stringA)
+    stringB = modifiedString(stringB)
+    if (stringA === stringB) return true
+    else return false
+}
 
 module.exports = anagrams;
